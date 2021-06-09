cd6d8dd5beede2f20f63eecc241a207c439e0f9f
V01D-NULL <timster23@protonmail.com>
2021-06-09 22:32:50 +0200

    [refactor] Update copyright notice

--- src/kernel/smp/spinlock.h
+++ src/kernel/smp/spinlock.h
@@ -1,3 +1,21 @@
+/*
+    This file is part of an AArch64 hobbyist OS for the Raspberry Pi 3 B+ called GENADEV_OS
+    Everything is openly developed on github: https://github.com/GENADEV/GENADEV_OS
+    Copyright (C) 2021  GENADEV_OS and it's affiliates
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+     
+    Author: Tim Thompson <https://github.com/V01D-NULL>
+*/
+
 #ifndef ATOMIC_LOCK_H
 #define ATOMIC_LOCK_H
 
